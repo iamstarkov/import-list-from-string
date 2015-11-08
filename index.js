426f1912f9811a90bd14507b19c5fb7a7b8f8d04
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-08 07:39:20 +0100

    indent fix

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import { pipe, prop, map, path } from 'ramda';
 const importList = pipe(
   string => parse(string, { ecmaVersion: 6, sourceType: 'module' }),
   prop('body'),
-  map(path(['source', 'value']))
-)
+  map(path(['source', 'value'])));
 
 export default function importListFromString(input) {
   if (!input) return;
