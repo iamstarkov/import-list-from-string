a4adbcbc3b48d6b1fff5fbdf4a0e25f5ac76a570
Vladimir Starkov <iamstarkov@gmail.com>
2015-11-08 07:29:27 +0100

    test: indent fix

--- test.js
+++ test.js
@@ -3,8 +3,7 @@ import importListFromString from './index';
 
 const input = `
 import out from 'out';
-import local from './local';
-`;
+import local from './local';`;
 
 it('should importListFromString', () =>
   deepEqual(importListFromString(input), ['out', './local']));
