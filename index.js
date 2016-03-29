f3824302551c0f99780f955d144d05b9efedad84
Vladimir Starkov <iamstarkov@gmail.com>
2016-03-29 16:55:58 +0200

    reimplement using walker

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { parse } from 'acorn';
+import { parse as _parse } from 'acorn';
+import { simple } from 'acorn/dist/walk';
 import R from 'ramda';
 
 // contract :: String -> Constructor -> a
@@ -7,15 +8,20 @@ const contract = R.curry((name, ctor, param) => R.unless(
   () => { throw new TypeError(`\`${name}\` should be \`${R.type(ctor())}\`, but got \`${R.type(param)}\``); }
 )(param));
 
-const log = console.log.bind(console);
+const parse = input => _parse(input, { sourceType: 'module' });
+const importValue = R.path(['source', 'value']);
 
 const importList = R.unary(R.pipe(
   contract('input', String),
-  string => parse(string, { sourceType: 'module' }),
-  R.prop('body'),
-  R.filter(R.propEq('type', 'ImportDeclaration')),
-  R.map(R.path(['source', 'value'])),
-  R.identity
+  parse,
+  ast => {
+    let list = [];
+    const addToList = item => { list = list.concat(item); };
+    simple(ast, {
+      ImportDeclaration: R.pipe(importValue, addToList)
+    });
+    return list;
+  }
 ));
 
 export default importList;
